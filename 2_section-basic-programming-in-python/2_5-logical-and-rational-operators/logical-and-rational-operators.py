a44e52993471f0b6b85da044d7d3360c1ae75c55
douglasmatosdev <douglasmatosdev@gmail.com>
2023-04-08 15:52:38 -0300

    logical and ratinal operatiors

---
+++ 2_section-basic-programming-in-python/2_5-logical-and-rational-operators/logical-and-rational-operators.py
@@ -0,0 +1,32 @@
+# Logical and rational operators
+print('# Logical and rational operators')
+a = True
+b = False
+
+print(f'a = {a}, b = {b}')
+
+print()
+
+print(f'a and b = {a and b}')
+print(f'a & b = {a & b}')
+print(f'c = a and b')
+
+c = a and b
+print("'A' e 'B' são iguais?", c)
+
+print('a or b =', a or b)
+print('a | b =', a | b)
+
+print('not a =', not a)
+print('not b =', not b)
+
+print()
+
+# Ralational operators
+print('# Ralational operators')
+print('5 > 3 = ', 5 > 3)
+print('5 < 3 = ', 5 < 3)
+print('5 >= 3 = ', 5 >= 3)
+print('5 <= 3 = ', 5 <= 3)
+print('5 == 3 = ', 5 == 3)
+print('5 != 3 = ', 5 != 3)
